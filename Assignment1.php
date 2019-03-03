6b2c8b2c8f1dc01e0ca3a505ae6385a6147a9a2e
Afnan A. Abed <haeseongp@gmail.com>
2019-03-03 23:46:53 +0200

    Assignment 1 solved

---
+++ Assignment1.php
@@ -0,0 +1,15 @@
+<!-- Create an associative array, the Indices are the titles of your 
+registered courses last semester and the values are your grades -->
+
+<?php
+	// Way 1
+	$grades = array("Web Development 1" => 99, "Data Structures" => 94, "Mathematics" => 95, 
+		"Software Engineering" => 98, "Calculus" => 89);
+
+	// Way 2
+	$grades2["Web Development 1"] = 99;
+	$grades2["Data Structures"] = 94;
+	$grades2["Mathematics"] = 95;
+	$grades2["Software Engineering"] = 98;
+	$grades2["Calculus"] = 89;
+?>
